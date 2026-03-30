894683aa591aa85406c560ff7fb77499ddc8ac7c
Nathan Fekade <nathanfekade276@gmail.com>
2026-03-30 10:02:41 +0300

    remove location from warden register

---
+++ prisma/migrations/20260330065808_remove_warden_location/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `currentLocation` on the `Warden` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Warden" DROP COLUMN "currentLocation";
